510c22bf55bc38da4756ef271cc1afc6e53a8989
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-03-21 16:49:52 +0100

    first iteration done

--- routes/employee.js
+++ routes/employee.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 function checkRoles(role) {
   return function(req, res, next) {
@@ -11,9 +13,57 @@ function checkRoles(role) {
     }
   }
 }
+router.get('/list', checkRoles('BOSS'),  (req, res, next) => {
+  User.find()
+    .then(users => {
+      res.render('employee/list', {users} )
+    })
+})
+
+router.get('/edit/:id', checkRoles('BOSS'), (req, res, next) =>{
+  User.findById(req.params.id)
+  .then(user => {
+    res.render('employee/info', user)
+  })
+})
+
+router.post('/info/:id', checkRoles('BOSS'), (req, res, next) =>{
+  User.updateOne({_id:req.params.id}, {role:req.body.role})
+  .then(() => {
+    res.redirect('/employee/list')
+  })
+  .catch(e => {console.log(e)})
+})
 
 router.get('/', checkRoles('BOSS'), (req, res, next) => {
   res.render('employee/add')
+})
+
+router.post('/add', checkRoles('BOSS'), (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+     return User.create({
+      username: req.body.username,
+      password: hash,
+      role: req.body.role
+    })
+    .then(user => {
+      res.send(user)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+})
+})
+
+router.get('/delete/:id',checkRoles('BOSS'), (req, res, next) =>{
+  User.deleteOne({_id:req.params.id})
+    .then( () => {
+      res.redirect('/employee/list')
+    })
+    .catch(e => {
+      console.log(e)
+    })
 })
 
 router.post('/login', passport.authenticate('local', {
@@ -21,4 +71,4 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/auth/login'
 }))
 
-module.exports = router
+module.exports = router;
